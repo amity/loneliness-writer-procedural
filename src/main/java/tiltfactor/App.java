7da10b2132674fdb6e60509bc9e92e40387540b7
soph-iest <natejneumann@gmail.com>
2018-10-12 18:01:06 -0400

    testing with storing objects in temp variables

--- src/main/java/tiltfactor/App.java
+++ src/main/java/tiltfactor/App.java
@@ -1,5 +1,12 @@
 package tiltfactor;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  * Hello world!
  */
@@ -14,7 +21,39 @@ public final class App {
      */
     public static void main(String[] args) {
         System.out.println("xd World!");
-        System.out.println(args[0]);
-
+        Writer myWriter = new Writer();
+        FileOutputStream fos;
+        ObjectOutputStream oos;
+		try {
+			fos = new FileOutputStream("mywriter2.tmp");
+			oos = new ObjectOutputStream(fos);	
+			oos.writeObject(myWriter);
+			oos.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        FileInputStream fis;
+        
+        Writer tempWriter;
+        try {
+        	fis = new FileInputStream("mywriter2.tmp");
+        	ObjectInputStream ois = new ObjectInputStream(fis);
+        	tempWriter = (Writer) ois.readObject();
+        	System.out.println(tempWriter.details.toString());
+        	ois.close();
+        } catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
--- src/main/java/tiltfactor/Writer.java
+++ src/main/java/tiltfactor/Writer.java
@@ -1,9 +1,84 @@
 package tiltfactor;
 
-public class Writer {
-	public char name;
+import java.io.Serializable;
+import java.io.StringWriter;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.HashMap;
+
+import org.jsefa.Serializer;
+import org.jsefa.csv.CsvIOFactory;
+import org.jsefa.csv.annotation.CsvDataType;
+import org.jsefa.csv.annotation.CsvField;
+
+public class Writer implements Serializable {
+	public String name;
+	public Writer partner;
+	public Date birthday;
+	public HashMap<String, String> details;
+	public String address;
 	
-	public Writer(char writerName) {
-		name = writerName;
+	@SuppressWarnings("deprecation")
+	public Writer() {
+		name = "Sophie";
+		partner = null;
+		birthday = new Date(1998, 1, 5);
+		details = new HashMap<String, String>();
+		details.put("book", "The Mysterious Benedict Society");
+		details.put("movie", "Spider-Man");
+		address = "2892 Hinman";
 	}
+	
+//	public Writer() {
+//		details = new HashMap<String, String>();
+//	}
+	
+	@CsvDataType() 
+	public class WriterSerial {
+		@CsvField(pos = 1)
+		String name;
+		
+		@CsvField(pos = 2, format="dd.MM.yyyy")
+		Date birthday;
+	}
+	
+	public void writeMe() {
+		Serializer serializer = CsvIOFactory.createFactory(WriterSerial.class).createSerializer();
+		StringWriter stringWriter = new StringWriter();
+		serializer.open(stringWriter);
+		serializer.write(this);
+	}
+	
+	
+	public String getName() {
+			return name;
+	}
+	
+	public void setName(String newName) {
+		name = newName;
+	}
+	
+	public Writer getPartner() {
+			return partner;
+	}
+	
+	public void setPartner(Writer newPartner) {
+		partner = newPartner;
+	}
+	
+	public Date getBirthday() {
+			return birthday;
+	}
+	
+	public void setBirthday(String newBirthday) {
+		DateFormat df = DateFormat.getInstance();
+		try {
+			birthday = df.parse(newBirthday);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
